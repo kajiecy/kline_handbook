dfe0b9d174d732a7282de6f3da29dd1bc03ead26
卡杰 <851416347@qq.com>
2022-09-27 16:27:59 +0800

    测试提交

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,8 @@
     />
 
     <div class="wrapper">
+      <a-input />
+      <TestComp />
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
@@ -19,6 +21,7 @@
 </template>
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
+import TestComp from "@/components/TestComp.vue";
 </script>
 
 <style scoped></style>
